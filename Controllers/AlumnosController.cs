30118f92b7420a24935aa002fc82613a06eb6538
Benjamin <hdmt.benjamin@gmail.com>
2021-11-25 17:07:10 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/AlumnosController.cs
@@ -0,0 +1,97 @@
+﻿using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TutorialLinQ.Models;
+
+namespace TutorialLinQ.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        AlumnoCLS modelo;
+
+        public AlumnosController()
+        {
+            modelo = new AlumnoCLS();
+        }
+
+        // GET: Lista
+        public ActionResult Lista()
+        {
+            List<Alumnos> listaalu = modelo.GetAlumno();
+
+            return View(listaalu);
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        public ActionResult Create(String APELLIDOS, String NOMBRES, int EDAD, String MAIL, String DOMICILIO, String TELEFONO, String DNI)
+        {
+            modelo.InsertarAlumno(APELLIDOS, NOMBRES, EDAD, MAIL, DOMICILIO, TELEFONO, DNI);
+            return RedirectToAction("Lista");
+        }
+
+        // GET: Delete
+        public ActionResult Delete(int ID)
+        {
+
+            modelo.EliminarAlumno(ID);
+            return RedirectToAction("Lista");
+        }
+
+        // GET: Edit
+        public ActionResult Edit(int ID)
+        {
+            Alumnos alu = modelo.DetalleAlumno(ID);
+            return View(alu);
+        }
+
+        // POST: Edit
+        [HttpPost]
+        public ActionResult Edit(int ID, String APELLIDOS, String NOMBRES, int EDAD, String MAIL, String DOMICILIO, String TELEFONO, String DNI)
+        {
+            modelo.EditarAlumno(ID, APELLIDOS, NOMBRES, EDAD, MAIL, DOMICILIO, TELEFONO, DNI);
+            return RedirectToAction("Lista");
+        }
+
+        // GET: Details
+        public ActionResult Details(int ID)
+        {
+            Alumnos alu = modelo.DetalleAlumno(ID);
+            return View(alu);
+        }
+
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Convert Index Page as PDF
+        public ActionResult PrintViewToPdf()
+        {
+            var report = new ActionAsPdf("Lista");
+            return report;
+        }
+
+        //Convert partial Page as PDF
+        public ActionResult PrintPartialViewToPdf(int id)
+        {
+            using (ITES_MVCEntities db = new ITES_MVCEntities())
+            {
+                Alumnos alumno = modelo.DetalleAlumno(id);
+                var report = new PartialViewAsPdf("~/Views/Shared/DetailEmployee.cshtml", alumno);
+                return report;
+            }
+        }
+
+    }
+}
---
+++ Models/AlumnoCLS.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TutorialLinQ.Models
+{
+    public class AlumnoCLS
+    {
+        ITES_MVCEntities contexto;
+
+        [Key]
+        public int idAlumno { get; set; }
+        [Required]
+        public string apellidos { get; set; }
+        [Required]
+        public string nombres { get; set; }
+        [Required]
+        public int edad { get; set; }
+        public string mail { get; set; }
+        public string domicilio { get; set; }
+        public string telefono { get; set; }
+        public string dni { get; set; }
+
+        public AlumnoCLS()
+        {
+            contexto = new ITES_MVCEntities();
+        }
+
+        public List<Alumnos> GetAlumno()
+        {
+            var listaalu = (from datos in contexto.Alumnos  
+                            select datos).ToList();
+            return listaalu;
+        }
+
+        public void InsertarAlumno(String apellidos, String nombres, int edad, String mail, String domicilio, String telefono, String dni)
+        {
+            Alumnos alu = new Alumnos();
+            alu.apellidos = apellidos;
+            alu.nombres = nombres;
+            alu.edad = edad;
+            alu.mail = mail;
+            alu.domicilio = domicilio;
+            alu.telefono = telefono;
+            alu.dni = dni;
+
+            contexto.Alumnos.Add(alu);
+
+            contexto.SaveChanges();
+        }
+
+        public void EditarAlumno(int lega, String apellidos, String nombres, int edad, String mail, String domicilio, String telefono, String dni)
+        {
+            Alumnos alu = this.DetalleAlumno(lega);
+
+            alu.apellidos = apellidos;
+            alu.nombres = nombres;
+            alu.edad = edad;
+            alu.mail = mail;
+            alu.domicilio = domicilio;
+            alu.telefono = telefono;
+            alu.dni = dni;
+
+            contexto.SaveChanges();
+        }
+
+        public Alumnos DetalleAlumno(int lega)
+        {
+            var alu = (from datos in contexto.Alumnos
+                       where datos.idAlumno == lega
+                       select datos).FirstOrDefault();
+
+            return alu;
+        }
+
+        public void EliminarAlumno(int ID)
+        {
+            try
+            {
+                Alumnos alu = new Alumnos();
+                alu = DetalleAlumno(ID);
+                contexto.Alumnos.Remove(alu);
+                contexto.Entry(alu).State = EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
